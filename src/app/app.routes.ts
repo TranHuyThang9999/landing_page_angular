cf414bd15db1eb6798b80db3f0be0b947173e1b0
Tran Huy Thang <tranhuythang9999@gmail.com>
2025-03-03 16:33:31 +0700

    Add home component and configure environment for development

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterFormComponent } from './register-form/register-form.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
-        path:'',component: AppComponent,
+        path:'',component: HomeComponent,
     },
     {
         path: 'register',component: RegisterFormComponent,

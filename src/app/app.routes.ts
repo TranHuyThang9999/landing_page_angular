bc276e655ddde46447c99dd9c53c36e44bfbd39b
Tran Huy Thang <tranhuythang9999@gmail.com>
2025-03-03 15:30:27 +0700

    Add registration form component and configure environment settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component: AppComponent,
+    },
+    {
+        path: 'register',component: RegisterFormComponent,
+    }
+];
---
+++ src/app/register-form/register-form.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../../../environment';
+import { response } from 'express';
+
+@Component({
+  selector: 'app-register-form',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
+})
+
+export class RegisterFormComponent {
+  user = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  constructor(private http: HttpClient) { }
+
+  registerUser() {
+    this.http.post(`${environment.apiUrl}/user/register`, this.user)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          alert('Đăng ký thành công!');
+        },
+        error: (error) => {
+          console.error('Lỗi khi đăng ký:', error);
+          alert('Đăng ký thất bại!');
+        }
+      });
+  }
+}
+
+

a139fd70f5a1f322bfc9ac9c711d6ed3eac1292d
Tran Huy Thang <tranhuythang9999@gmail.com>
2025-03-05 00:32:24 +0700

    Refactor file upload component structure and implement file upload functionality with presigned URLs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { HomeComponent } from './home/home.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +10,5 @@ export const routes: Routes = [
     {
         path: 'register',component: RegisterFormComponent,
     },
-  
+
 ];

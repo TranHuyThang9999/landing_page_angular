2d675b9e4a354a96d926633ae61641cbc72b3704
Tran Huy Thang <tranhuythang9999@gmail.com>
2025-03-05 14:56:30 +0700

    Add profile component with user data fetching and display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterFormComponent } from './pages/register-form/register-form.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,8 @@ export const routes: Routes = [
     },
     {
         path: 'login', component: LoginComponent,
+    },
+    {
+        path:'profile',component:ProfileComponent,
     }
-
 ];
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FetchApiInstanceService } from '../utils/fetch_api.service';
+
+interface UserProfile {
+  id: number;
+  name: string;
+  email: string;
+  avatarUrl: string;
+}
+
+interface ResponseData {
+  code: number;
+  message: string;
+  data: UserProfile;
+}
+
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  userProfile: UserProfile | null = null;
+
+  constructor(
+    private apiService: FetchApiInstanceService,
+  ) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  async getData() {
+    try {
+      const response: ResponseData = await this.apiService.get('user/profile');
+      if (response.code === 0) {
+        this.userProfile = response.data;
+      }
+    } catch (error) {
+      console.error("Lỗi khi lấy dữ liệu người dùng:", error);
+    }
+  }
 
 }

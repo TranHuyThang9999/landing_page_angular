87d44506f55e0b27628c4ee128b2fea93ec1b200
Tran Huy Thang <tranhuythang9999@gmail.com>
2025-03-05 10:34:32 +0700

    Add login component and update routing; enhance register form with login link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterFormComponent } from './pages/register-form/register-form.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,8 @@ export const routes: Routes = [
     {
         path: 'register',component: RegisterFormComponent,
     },
+    {
+        path: 'login', component: LoginComponent,
+    }
 
 ];

8303796fe54d9c023a57debab3301ca503d59ee2
Tran Huy Thang <tranhuythang9999@gmail.com>
2025-03-07 01:09:46 +0700

    Add loading template to admin dashboard and manage loading state

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { ProfileComponent } from "../profile/profile.component";
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { CreateTicketComponent } from '../components/create-ticket/create-ticket.component';
 import { UserListComponent } from "../user-list/user-list.component";
 import { TicketListComponent } from "../components/ticket-list/ticket-list.component";
 import { TicketAssignmentComponent } from "../components/ticket-assignment/ticket-assignment.component";
 import { TicketsAssignedToMeComponent } from "../components/tickets-assigned-to-me/tickets-assigned-to-me.component";
-
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
@@ -18,11 +17,17 @@ import { TicketsAssignedToMeComponent } from "../components/tickets-assigned-to-
     UserListComponent,
     TicketListComponent,
     TicketAssignmentComponent,
-    TicketsAssignedToMeComponent
+    TicketsAssignedToMeComponent,
+    NzSpinModule
 ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
-
+  isLoaded = false;
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 2000);
+  }
 }
